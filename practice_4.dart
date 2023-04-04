c6280f32a3465783708f9cff2d5d3abb01dba213
Amira <kenenbaeva01@gmail.com>
2023-04-05 00:05:00 +0600

    выполнила практику 4

---
+++ practice_4.dart
@@ -0,0 +1,75 @@
+
+void main() {
+
+  // 1
+  int num1 = 9;
+    if (num1 == 10) {
+      print("Верно");
+    } else {
+      print("Неверно");
+    }
+  
+  // 2
+  int num2 = 0;
+    if (num2==0) {
+      print("Верно");
+    } else {
+      print("Неверно");
+    }
+  
+  // 3
+  int num3 = -2;
+    if (num3 > 0) {
+      print("Верно");
+    } else {
+      print("Неверно");
+    }
+
+  // 4 
+  int num4 = -3;
+    if (num4 < 0) {
+      print("Верно");
+    } else {
+      print("Неверно");
+    }
+  // 5
+  int num5 = 1;
+    if (num5 >= 0) {
+      print("Верно");
+    } else {
+      print("Неверно");
+    }
+
+  // 6
+   int num6 = 0;
+    if (num6 <= 0) {
+      print("Верно");
+    } else {
+      print("Неверно");
+    }
+  
+  // 7
+   int num7 = 0;
+    if (num7 != 0) {
+      print("Верно");
+    } else {
+      print("Неверно");
+    }
+
+  //8
+  String num8 = "test";
+    if ( num8 == "test" ) {
+      print("Верно");
+    } else {
+      print("Неверно");
+    }
+  
+  // 9
+
+  String num9 = "0";
+    if ( num9 == "1" && num9 is String ) {
+      print("Верно");
+    } else {
+      print("Неверно");
+    }
+}
